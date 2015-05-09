ed59fc87252bf4ba217e0877ab5c861efd5a4959
nikhil <nkmittal4994@gmail.com>
2015-05-10 00:14:01 +0530

    Time difference calculated!

--- Synchronize/client.py
+++ Synchronize/client.py
@@ -1,8 +1,16 @@
 import socket
+import time
 import pickle
 s = socket.socket()
 host = socket.gethostname()
 port = 12345
 s.connect((host, port))
 
-print pickle.loads(s.recv(1024))
+while True:
+    data = pickle.loads(s.recv(1024))
+    data['receive'] = time.ctime()
+    time.sleep(10)
+    data['transmit'] = time.ctime()
+    s.send(pickle.dumps(data))
+    time_diff = pickle.loads(s.recv(1024))
+    print "Time difference between Server and Client clocks:", time_diff
--- Synchronize/server.py
+++ Synchronize/server.py
@@ -1,14 +1,35 @@
-import socket, time
+import socket
+import time
 import pickle
+import datetime as dt
+
+
+def calc(start, end):
+    start_dt = dt.datetime.strptime(start[11:19], '%H:%M:%S')
+    end_dt = dt.datetime.strptime(end[11:19], '%H:%M:%S')
+    diff = (end_dt - start_dt)
+    return diff.seconds / 60
+
 s = socket.socket()
 host = socket.gethostname()
 port = 12345
 s.bind((host, port))
 s.listen(5)
 data = {}
+
+
 while True:
     c, addr = s.accept()
     print 'Got connection from', addr
     # msg = raw_input("enter plaintext:")
-    data['orignal'] = time.ctime()
+    data['original'] = time.ctime()
     c.send(pickle.dumps(data))
+    data = pickle.loads(c.recv(1024))
+    data['returned'] = time.ctime()
+    print(data)
+    sending_time = calc(data['receive'], data['original'])
+    receiving_time = calc(data['returned'], data['transmit'])
+    # Round-trip time
+    rtt = sending_time + receiving_time
+    time_diff = calc(data['receive'], data['original']) - rtt / 2
+    c.send(pickle.dumps(time_diff))
