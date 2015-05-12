b8ab2d673c63f89ec8c3fd6dfa465ae9a61c6e04
nikhil <nkmittal4994@gmail.com>
2015-05-12 21:13:49 +0530

    file transfer from client to server :file_folder:

--- File encryption/client.py
+++ File encryption/client.py
@@ -1,14 +1,17 @@
-import socket 
+import socket
 
 s = socket.socket()
 host = socket.gethostname()
-port = 12345 
+port = 12345
 s.connect((host, port))
-f = open('test_recv.txt','wb')
-l = c.recv(1024)
-    while (l):
-        print "Receiving..."
-        f.write(l)
-        l = c.recv(1024)
-    f.close()
-print s.recv(1024)
+f = open('test/test.txt', 'rb')
+l = f.read(1024)
+print l
+while (l):
+    # Send file.
+    # TODO:Encrypt this.
+    s.send(l)
+    l = f.read(1024)
+    print l
+f.close()
+s.close()
--- File encryption/server.py
+++ File encryption/server.py
@@ -4,17 +4,18 @@ s = socket.socket()
 host = socket.gethostname()
 port = 12345
 s.bind((host, port))
-f.open('test.txt','wb')
+f = open('test/test_recv.txt', 'wb')
 s.listen(5)
 while True:
     c, addr = s.accept()
     print 'Got connection from', addr
-    # Send file.
-    print 'Sending...'
-    l = f.read(1024)
-    while l:
-    	print 'Sending..'
-    	c.send(l)
-    	l = f.read(1024)
-    f.close()
-    print 'Done Sending.'
+    # Receive file.
+    while True:
+        l = c.recv(1024)
+        if l:
+            f.write(l)
+        else:
+            f.close()
+            break
+    print 'Done Receiving.'
+    c.close()
