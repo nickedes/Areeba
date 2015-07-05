6cf67c9c9a988215d1e469e5a793778e5bcb8f42
nikhil <nkmittal4994@gmail.com>
2015-07-05 22:41:19 +0530

    2 tests, new one for checking negative no.s prime or not? :smiley: Both test ran successfully :tada:

--- RSA/test.py
+++ RSA/test.py
@@ -7,9 +7,15 @@ class RSATestCase(unittest.TestCase):
     """ Tests for `encrypt.py`. """
 
     # First we will test the prime no. module.
-    def test_is_four_prime(self):
-        """Is Four successfully determined to be prime?"""
-        self.assertTrue(is_prime(4))
+    def test_is_five_prime(self):
+        """Is five successfully determined to be prime?"""
+        self.assertTrue(is_prime(5))
+
+    def test_negative_number(self):
+        """Is a negative number correctly determined not to be prime?"""
+        for index in range(-1, -10, -1):
+            self.assertFalse(is_prime(index))
+
 
 if __name__ == '__main__':
     unittest.main()
