26fc03a537b780a20ebe4ef93b29ad2963a68141
nikhil <nkmittal4994@gmail.com>
2015-07-05 22:24:58 +0530

    first unittest, which tests for is_prime module of encryption in RSA.

--- RSA/test.py
+++ RSA/test.py
@@ -7,9 +7,9 @@ class RSATestCase(unittest.TestCase):
     """ Tests for `encrypt.py`. """
 
     # First we will test the prime no. module.
-    def test_is_five_prime(self):
-        """Is five successfully determined to be prime?"""
-        self.assertTrue(is_prime(5))
+    def test_is_four_prime(self):
+        """Is Four successfully determined to be prime?"""
+        self.assertTrue(is_prime(4))
 
 if __name__ == '__main__':
     unittest.main()
